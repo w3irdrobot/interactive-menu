de49cc89e58a48117f94e0c9ec8ad0243620f31c
Alex Sears <alexwsears@gmail.com>
2016-02-28 18:44:03 -0500

    Add items to cart UI on ITEM_ADDED

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -2,21 +2,15 @@ import { addClass, addId, button, div, h1, i, p, section, text, ul } from '../bu
 import { $ } from '../helpers';
 import modalItem from './modalItem';
 
-export default function modal(store, items = []) {
+export default function modal(store) {
   const close = addId(addClass(i(), 'fa', 'fa-times', 'close'), 'close');
   const title = addClass(h1(text('Cart')), 'title');
 
-  let cart;
-  if (items.length === 0) {
-    cart = p(text('Your cart is empty.'));
-  } else {
-    const cartItems = items.map(modalItem);
-    cart = addClass(ul(...cartItems), 'menu');
-  }
+  const cartContainer = addId(div(p(text('Your cart is empty.'))), 'cart-items');
 
   const checkoutButton = addClass(button(text('Checkout')), 'button', 'is-fullwidth');
 
-  const modalContainer = addClass(div(close, title, cart, checkoutButton), 'modal-container');
+  const modalContainer = addClass(div(close, title, cartContainer, checkoutButton), 'modal-container');
 
   const modalEle = addId(addClass(section(modalContainer), 'modal'), 'modal');
 
@@ -29,8 +23,11 @@ export default function modal(store, items = []) {
     }
   });
 
-  store.on('ITEM_ADDED', ({ items, cart}) => {
-
+  store.on('ITEM_ADDED', ({ items, cart }) => {
+    const cartArray = [...cart];
+    const cartItems = cartArray.map(itemId => modalItem(items[itemId]));
+    const cartList = addClass(ul(...cartItems), 'menu');
+    $('#cart-items').children(cartList);
   });
 
   return modalEle;
